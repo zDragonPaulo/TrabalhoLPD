6e121adb56c52db02adb00069a7b32fecebeb995
zDragonPaulo <23919@stu.ipbeja.pt>
2025-10-19 23:35:46 +0100

    Criação de versões de Menu

---
+++ menu_cli.py
@@ -0,0 +1,7 @@
+print("Bem vindo a este projeto. \n Desenvolvido por Paulo Abade - 23919\n")
+
+print("Escolha uma opção.\n")
+print(" 1 - Port Checking \n 2 - DoS \n 3 - SYN Flood \n 4 - Analisar Logs \n 5 - Troca de Mensagens \n 6 - Port Knocking \n 7 - Gestor de Passwords \n 0 - Sair")
+option = -1
+while(option != 0):
+    option = int(input("Escolha uma opção válida.\n"))

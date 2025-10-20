1066f0ffc2b3010749d9d0a4bda3546c8b615243
zDragonPaulo <23919@stu.ipbeja.pt>
2025-10-20 16:34:43 +0100

    Reorganização do Repositório

---
+++ src/menu_cli.py
@@ -0,0 +1,34 @@
+print("Bem vindo a este projeto. \n Desenvolvido por Paulo Abade - 23919\n")
+def main_menu():
+    print(" 1 - Port Checking \n 2 - DoS \n 3 - SYN Flood \n 4 - Analisar Logs \n 5 - Troca de Mensagens \n 6 - Port Knocking \n 7 - Gestor de Passwords \n 0 - Sair")
+    option = -1
+    while(option != 0):
+        answer = input("Escolha uma opção: ")
+
+        match answer:
+
+            case "1":
+                print("Portchecking")
+            case "2":
+                print("DoS")
+            case "3":
+                print("SYN Flood")
+            case "4":
+                print("Analisar Logs")
+            case "5":
+                print("TODO Chamar o ficheiro do Troca de Mensagens")
+            case "6":
+                print("TODO Chamar o ficheiro do PortKnocking")
+            case "7":
+                print("TODO Chamar o ficheiro do Gestor de Passwords")
+            case "0":
+                print("Saindo..")
+                option = 0
+            case _:
+                print("Escolha uma opção válida. Entre 1 a 7 ou 0 para sair")
+    # Start again
+main_menu()
+
+
+
+

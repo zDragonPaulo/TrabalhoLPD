29872d44ace7946f3da7930bd0615a5793413d2b
zDragonPaulo <23919@stu.ipbeja.pt>
2025-10-20 18:20:00 +0100

    Preparação para o DoS

---
+++ src/AttackFiles/dos_attack.py
@@ -0,0 +1,7 @@
+# Paulo Abade - 23919
+# Mestrado de Engenharia de Segurança Informática
+# Iniciado em 20/10/2025
+# Última atualização: 20/10/2025
+
+# Faz um ataque de SYN Flood a um IP e a uma porta que tenham sido especificados pelo utilizador
+# Foi seguido o tutorial de: https://github.com/M-Taghizadeh/PyDOS

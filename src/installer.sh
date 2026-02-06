59ff122737d43c191da70d0a1164eef8329ce2ee
zDragonPaulo <23919@stu.ipbeja.pt>
2026-02-06 18:43:38 +0000

    Relatório adiantado, faltam as fotos

--- src/installer.sh
+++ src/installer.sh
@@ -4,9 +4,10 @@ echo "--- A instalar dependências para o Projeto ---"
 
 # Atualizar a lista de pacotes do sistema
 sudo apt update
-
 # Garantir que o Python3 e o PIP estão instalados
-sudo apt install -y python3 python3-pip
+sudo apt install -y python3 python3-pip rsyslog apache2
+sudo systemctl start --now rsyslog apache2
+
 
 # Instalar as bibliotecas do requirements.txt
 # O --break-system-packages é necessário em versões novas do Kali/Debian
